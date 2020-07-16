75ef7b0f825033b1b4bdc231644cc86c494a26a3
Yesid <trigoperfec3@hotmail.com>
2020-07-16 13:23:32 -0500

    Adiciona permisos de gps a manifiest

---
+++ app/src/main/java/com/mitiempo/pruebaolimpiaitandroid/Presentacion/Pantalla3/ManejadorGPS.kt
@@ -0,0 +1,13 @@
+package com.mitiempo.pruebaolimpiaitandroid.Presentacion.Pantalla3
+
+import android.content.Context
+
+class ManejadorGPS(
+    private val context: Context
+)
+{
+
+    fun verificarPermisosGPS(){
+
+    }
+}
