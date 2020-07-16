2da67a497d4ccdbd8bbcf325f3a9dace9c65b44f
Yesid <trigoperfec3@hotmail.com>
2020-07-16 13:16:20 -0500

    Adicion de provider encargado de recopilar respuesta desde la camara y la galeria

--- app/src/main/java/com/mitiempo/pruebaolimpiaitandroid/Presentacion/MainActivity.kt
+++ app/src/main/java/com/mitiempo/pruebaolimpiaitandroid/Presentacion/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mitiempo.pruebaolimpiaitandroid.Presentacion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mitiempo.pruebaolimpiaitandroid.Presentacion.Dialogos.DialogoGenerico
@@ -12,12 +13,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        hola_mundo.setOnClickListener {
-            mostrarDialogoDetallado(
-                R.string.almacenamiento_nube,
-                R.string.error_de_conexion,
-                DialogoGenerico.TipoDialogo.ERROR
-            )
-        }
+
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        selector_foto.conOnRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        selector_foto.escuchaOnActivityResult(requestCode, resultCode, data)
+    }
+
 }
