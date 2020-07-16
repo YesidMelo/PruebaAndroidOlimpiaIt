041f6cb5c81b0a70772e938f10ea5bf05af31cbf
Yesid <trigoperfec3@hotmail.com>
2020-07-16 12:08:00 -0500

    Creacion de formulario primera pantalla

---
+++ app/src/main/java/com/mitiempo/pruebaolimpiaitandroid/Presentacion/Pantalla1/FormularioRegistro.kt
@@ -0,0 +1,14 @@
+package com.mitiempo.pruebaolimpiaitandroid.Presentacion.Pantalla1
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class FormularioRegistro @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+}
